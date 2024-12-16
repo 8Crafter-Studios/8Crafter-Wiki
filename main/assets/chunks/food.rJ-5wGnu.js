593f7e84878340fa9a4897140c057ac55b9a81bd
Andexter8 <134829412+Andexter8@users.noreply.github.com>
2024-12-16 01:31:43 +0000

    Deploying to generated from @ 8Crafter-Studios/8Crafter-Wiki@bd4b9b5ac19fc37cae9466b611cf61aa2065d1bf 🚀

---
+++ main/assets/chunks/food.rJ-5wGnu.js
@@ -0,0 +1,1 @@
+const s="/8Crafter-Wiki/main/assets/images/concepts/emojis/hud/food.png";export{s as _};
---
+++ main/assets/chunks/gametick.HTS3KL7m.js
@@ -0,0 +1,1 @@
+const s="/8Crafter-Wiki/main/assets/images/commands/gametick.png";export{s as _};
