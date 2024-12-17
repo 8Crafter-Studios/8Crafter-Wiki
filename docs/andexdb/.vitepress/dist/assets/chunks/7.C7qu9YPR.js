4c93cfa9ad5476570642418143d0be18ed1556e6
Andexter8 <8crafteryt@gmail.com>
2024-12-16 17:35:06 -0800

    Bug Fix

--- docs/andexdb/.vitepress/dist/assets/chunks/7.C7qu9YPR.js
+++
@@ -1,1 +0,0 @@
-const a="/8Crafter-Wiki/andexdb/assets/images/commands/commandBlockChain/7.png";export{a as _};
--- docs/andexdb/.vitepress/dist/assets/chunks/gametick.DL7JHEqe.js
+++
@@ -1,1 +0,0 @@
-const s="/8Crafter-Wiki/andexdb/assets/images/commands/gametick.png";export{s as _};
