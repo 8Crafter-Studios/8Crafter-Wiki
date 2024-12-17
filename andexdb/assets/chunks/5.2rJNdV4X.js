526994c554eedbd98b37a57f63823a9d92910e7a
Andexter8 <134829412+Andexter8@users.noreply.github.com>
2024-12-17 01:36:21 +0000

    Deploying to generated from @ 8Crafter-Studios/8Crafter-Wiki@4c93cfa9ad5476570642418143d0be18ed1556e6 🚀

--- andexdb/assets/chunks/5.2rJNdV4X.js
+++
@@ -1,1 +0,0 @@
-const a="/8Crafter-Wiki/andexdb/assets/images/commands/commandBlockChain/5.png";export{a as _};
--- andexdb/assets/chunks/food.BdRi9w0J.js
+++
@@ -1,1 +0,0 @@
-const s="/8Crafter-Wiki/andexdb/assets/images/concepts/emojis/hud/food.png";export{s as _};
