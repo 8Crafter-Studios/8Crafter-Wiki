5cf1607aeabf17474108b86496da80c3ad7325fc
8Crafter (PC) <8crafteryt@gmail.com>
2025-10-30 11:15:45 -0700

    Added more redirects.

--- docs/andexdb/.vitepress/redirects.ts
+++ docs/andexdb/.vitepress/redirects.ts
@@ -3,4 +3,7 @@ import { Redirects } from "./theme";
 // Alphabetical order please :)
 export default {
   "/andexdb/general/debug-sticks": "/andexdb/general/server-utilities",
+  "/andexdb/items/data-pick-block-stick": "/andexdb/items/pick-block-stick",
+  "/andexdb/items/liquid-clipped-pick-block-stick": "/andexdb/items/pick-block-stick",
+  "/andexdb/items/liquid-clipped-data-pick-block-stick": "/andexdb/items/pick-block-stick",
 } satisfies Redirects;
