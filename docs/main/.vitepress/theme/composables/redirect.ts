3f42faff8c2580b1fe61a87c7bfb701f1ebe7798
8Crafter (PC) <8crafteryt@gmail.com>
2025-10-30 11:11:14 -0700

    Fixed a bug where redirects would append `.html` to the end of the resulting URL.

--- docs/main/.vitepress/theme/composables/redirect.ts
+++ docs/main/.vitepress/theme/composables/redirect.ts
@@ -2,18 +2,21 @@ import { onBeforeMount } from "vue";
 import { useRouter } from "vitepress";
 import useData from "./data";
 
-export default function useRedirect() {
+export default function useRedirect(): void {
   const { theme } = useData();
 
   const router = useRouter();
 
-  function redirect() {
-    const from = router.route.path.replace(".html", "");
+  function redirect(): void {
+    const from: string = router.route.path.replace(/\.html$/, "");
 
-    const to = theme.value.redirects[from];
+    const to: string = theme.value.redirects[from];
     if (to === undefined) return;
 
-    router.go(to);
+    router.go(to).then((): void => {
+      // This removes the trailing `.html` from the URL.
+      if (location.href !== to) history.replaceState({}, "", location.href.replace(/\.html$/, ""));
+    });
   }
 
   onBeforeMount(redirect);
